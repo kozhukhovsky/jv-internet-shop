aaf23f5fc2d038a3af37e145ed990895c93bc5b5
Anton Kozhukhovsky <a.kozhukhovsky@gmail.com>
2019-09-17 17:18:47 +0300

    refactored Injector

---
+++ src/main/java/mate/academy/internetshop/lib/AnnotatedClassMap.java
@@ -0,0 +1,32 @@
+package mate.academy.internetshop.lib;
+
+import java.util.HashMap;
+import java.util.Map;
+import mate.academy.internetshop.dao.BucketDao;
+import mate.academy.internetshop.dao.ItemDao;
+import mate.academy.internetshop.dao.OrderDao;
+import mate.academy.internetshop.dao.UserDao;
+import mate.academy.internetshop.factory.Factory;
+import mate.academy.internetshop.service.BucketService;
+import mate.academy.internetshop.service.ItemService;
+import mate.academy.internetshop.service.OrderService;
+import mate.academy.internetshop.service.UserService;
+
+public class AnnotatedClassMap {
+    private static final Map<Class, Object> classMap = new HashMap<>();
+
+    static {
+        classMap.put(ItemDao.class, Factory.getItemDao());
+        classMap.put(BucketDao.class, Factory.getBucketDao());
+        classMap.put(OrderDao.class, Factory.getOrderDao());
+        classMap.put(UserDao.class, Factory.getUserDao());
+        classMap.put(ItemService.class, Factory.getItemService());
+        classMap.put(BucketService.class, Factory.getBucketService());
+        classMap.put(OrderService.class, Factory.getOrderService());
+        classMap.put(UserService.class, Factory.getUserService());
+    }
+
+    public static Object getImplementation(Class interfaceClass) {
+        return classMap.get(interfaceClass);
+    }
+}
