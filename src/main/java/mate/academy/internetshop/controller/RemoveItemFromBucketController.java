e1e5ce67d68e0469228ef7b9ff256b82b7dfa532
Anton Kozhukhovsky <a.kozhukhovsky@gmail.com>
2019-09-18 19:09:41 +0300

    fixed my very big mistake

--- src/main/java/mate/academy/internetshop/controller/RemoveItemFromBucketController.java
+++ src/main/java/mate/academy/internetshop/controller/RemoveItemFromBucketController.java
@@ -12,7 +12,7 @@ import mate.academy.internetshop.service.ItemService;
 
 @WebServlet("/removeItem")
 public class RemoveItemFromBucketController extends HttpServlet {
-    private static final long TEMP_USER_ID = 0L;
+    private static final long TEMP_BUCKET_ID = 0L;
 
     @Inject
     private static BucketService bucketService;
@@ -23,7 +23,7 @@ public class RemoveItemFromBucketController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         Long itemId = Long.valueOf(req.getParameter("item_id"));
-        bucketService.removeItemFromBucket(TEMP_USER_ID, itemId);
+        bucketService.removeItemFromBucket(TEMP_BUCKET_ID, itemId);
 
         resp.sendRedirect(req.getContextPath() + "/bucket");
     }
